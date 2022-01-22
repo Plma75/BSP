217204e98dcf22c0a098f3daa882084a403b4c67
Plma75 <71132499+Plma75@users.noreply.github.com>
2022-01-22 15:27:32 +0900

    UpdateChecker

--- src/main/java/com/plma75/bsp/Utils/UpdateChecker.java
+++ src/main/java/com/plma75/bsp/Utils/UpdateChecker.java
@@ -1,7 +1,36 @@
 package com.plma75.bsp.Utils;
 
+import com.plma75.bsp.BSP;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Scanner;
+import java.util.function.Consumer;
+
 public class UpdateChecker {
 
+    private final JavaPlugin plugin;
+    private final int resourceId;
 
+    public UpdateChecker(JavaPlugin plugin, int resourceId) {
+        this.plugin = plugin;
+        this.resourceId = resourceId;
+    }
+
+    public void getVersion(final Consumer<String> consumer) {
+        Bukkit.getScheduler().runTaskAsynchronously(this.plugin, () -> {
+            try (InputStream inputStream = new URL("https://api.spigotmc.org/legacy/update.php?resource=" + this.resourceId).openStream(); Scanner scanner = new Scanner(inputStream)) {
+                if (scanner.hasNext()) {
+                    consumer.accept(scanner.next());
+                }
+            } catch (IOException exception) {
+                Bukkit.getConsoleSender().sendMessage(BSP.getInstance().getPrefix() + ChatColor.RED + "Unable to check for update: " + exception.getMessage());
+            }
+        });
+    }
 
 }
